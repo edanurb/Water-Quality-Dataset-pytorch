b9f30aa07c528904c8ea8df0b437c413d3b84326
edanur <eda@gmail.co>
2021-08-13 19:27:57 +0300

    update csv file

--- main.py
+++ main.py
@@ -5,30 +5,30 @@ import torch.nn as nn
 from torch.utils.data import TensorDataset
 #data=torch.tensor(pd.read_csv("water_potability.csv").dropna().values,requires_grad=True)
 
-data=pd.read_csv("water_potability.csv").dropna()
+data=pd.read_csv("heart.csv").dropna()
 
-dataset=TensorDataset(torch.tensor(data.iloc[:,:9].values,dtype=torch.float32),torch.tensor(data.iloc[:,9:].values.reshape([2011]),dtype=torch.long))
-
-
-a=data.iloc[:,:9].values
+dataset=TensorDataset(torch.tensor(data.iloc[:,:13].values,dtype=torch.float32),torch.tensor(data.iloc[:,13:].values.reshape([303]),dtype=torch.long))
 
 
-train , val , test =random_split(dataset,[1407,402,202]) 
+
+
+train , val , test =random_split(dataset,[242,31,30]) 
 
 from torch.utils.data import DataLoader
 batch_size = 64
 train_dl=DataLoader(train,batch_size,shuffle=True)
 val_dl=DataLoader(train,batch_size)
+test_dl=DataLoader(test,batch_size)
 
 
 def acc(outputs,preds): 
     probility,pred=torch.max(preds,dim=1)
+    return torch.tensor(torch.sum(outputs==pred).item()/len(pred))
 
-    return torch.tensor(torch.sum(outputs==pred)).item()/len(outputs)
-
-def fit(epoch_size,train_dl,val_dl,model,loss_func):
-    opt=torch.optim.SGD(model.parameters(),lr=1e-6)
-    accurcy=0
+def fit(epoch_size,train_dl,val_dl,model):
+    opt=torch.optim.SGD(model.parameters(),lr=1e-5)
+    accurcy=[]
+    
     for epoch in range(epoch_size):
         #train
         for x,y in train_dl:
@@ -36,9 +36,7 @@ def fit(epoch_size,train_dl,val_dl,model,loss_func):
             preds=model(x)
            
             #calculate loss 
-            #print(preds)
-            #print(y)
-            loss=loss_func(preds,y)
+            loss=nn.functional.cross_entropy(preds,y)
             
             #graident descent
             loss.backward()
@@ -48,16 +46,26 @@ def fit(epoch_size,train_dl,val_dl,model,loss_func):
             
             #reset parameters
             opt.zero_grad()
+        if(epoch%10000==0):
+            print("loss : ", loss.item())
+        
         
         for x,y in val_dl:
             preds=model(x)
-
-            accurcy=acc(y,preds)
-        print(accurcy)     
+            accurcy.append(acc(y,preds))
+        if(epoch%10000==0):
+            print("acc: ",torch.stack(accurcy).mean())
+       
             
-model=nn.Linear(9,2)
-loss_func=nn.functional.cross_entropy
+model=nn.Linear(13,2)
 
-fit(5,train_dl,val_dl,model,loss_func)
+fit(100000,train_dl,val_dl,model)
+
+
+for x,y in test_dl:
+    prob,pred=torch.max(model(x), dim=1)
+    print("tahmin: " , pred)
+    print("gerçek: ",y)
+    print(torch.tensor(torch.sum(y==pred)/len(pred)))
 
 
